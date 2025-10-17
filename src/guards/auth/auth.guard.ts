18aa6e9e2f5950513558fb5d5869f40e72a39067
Silhaut <mnstirit@gmail.com>
2025-10-17 21:49:02 +0500

    feat(auth): add auth guard (#11)

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from '../../auth/constants';
+import { JwtPayloadDto } from '../../data/dto/jwt-payload.dto';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException('Missing token');
+
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayloadDto>(token, {
+        secret: jwtConstants.secret,
+      });
+
+      console.log('JWT PAYLOAD:', payload);
+
+      request.user = payload;
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
