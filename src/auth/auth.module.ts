f3cb7d35b417eb9cd271e22961c998061fda34c0
Silhaut <mnstirit@gmail.com>
2025-10-17 21:36:42 +0500

    feat(auth): create kwt secret and configure jwt module in (#11)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthUpdate } from './auth.update';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '5m' },
+    }),
+  ],
   providers: [AuthService, AuthUpdate],
   controllers: [AuthController],
 })
